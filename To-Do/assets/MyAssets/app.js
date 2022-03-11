3324b5e72ff5014a3b43dc5cbd2fd3b8d1da70ad
yucedemirayak <yuce.dmryk@gmail.com>
2022-03-11 20:08:37 +0300

    Task Unique Id's added

--- To-Do/assets/MyAssets/app.js
+++ To-Do/assets/MyAssets/app.js
@@ -2,7 +2,15 @@ let todoTask = {
   title: "",
   description: "",
   isCompleted: false,
+  taskId: null,
 };
+
+let minId = 100000;
+let maxId = 999999;
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 let taskList = [];
 
@@ -26,13 +34,16 @@ const clearTaskList = () => {
 } 
 
 const checkTheTask = (clicked_id) => {
-    let taskId = parseInt(clicked_id.split("-")[1]) - 1;
-    if (taskList[taskId].isCompleted) {
-        taskList[taskId].isCompleted = false;
+    let clickedTaskId = parseInt(clicked_id.split("-")[1]);
+    let clickedTask = taskList.find(obj => {return obj.taskId === clickedTaskId}) ;
+    
+    if (clickedTask.isCompleted) {
+      clickedTask.isCompleted = false;
     }
     else {
-        taskList[taskId].isCompleted = true;
+      clickedTask.isCompleted = true;
     }
+
     localStorage.setItem("taskListLS", JSON.stringify(taskList));
     document.getElementById("todo-list").innerHTML= '';
     listTasks();
@@ -43,6 +54,7 @@ const createTask = () => {
   task.title = document.getElementById("new-task-title").value;
   task.description = document.getElementById("new-task-description").value;
   task.isCompleted = false;
+  task.taskId = getRndInteger(minId, maxId);
 
   document.getElementById("add-task-container").reset();
 
@@ -54,7 +66,7 @@ const addTaskItemToTaskListHtml = (task, i) => {
   const taskListEl = document.getElementById("todo-list");
   const taskItemHtml =
   `<li class="todo-record ${isChecked[task.isCompleted]}">
-        <button class="todo-checkbox" onclick="checkTheTask(this.id)" id="task-${i}"></button>
+        <button class="todo-checkbox" onclick="checkTheTask(this.id)" id="task-${task.taskId}"></button>
         <div class="todo-record-text">
             <h2>${task.title}</h2>
             <h3>${task.description}</h3>
